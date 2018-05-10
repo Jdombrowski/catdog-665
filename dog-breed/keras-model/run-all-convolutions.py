1ab1845244f2455250032d76dba0369f193e6266
jondo <jdombrowski416@gmail.com>
2018-05-10 08:58:19 -0700

    adding in results from overnight testing

--- dog-breed/keras-model/run-all-convolutions.py
+++ dog-breed/keras-model/run-all-convolutions.py
@@ -1,8 +1,7 @@
 import os
 
 # Pure Convolutional Testing 
-os.system("python3 cnn_d1.py 1 1 90")
-os.system("python3 cnn_d1.py 1 2 90")
-os.system("python3 cnn_d1.py 1 4 90")
-os.system("python3 cnn_d1.py 1 8 90")
-os.system("python3 cnn_d1.py 1 16 90")
+os.system("python3 cnn_d1.py 0 1 90")
+os.system("python3 cnn_d1.py 0 2 90")
+os.system("python3 cnn_d1.py 0 4 90")
+os.system("python3 cnn_d1.py 0 8 90")
--- dog-breed/keras-model/run-all-image-sizes.py
+++ dog-breed/keras-model/run-all-image-sizes.py
@@ -1,6 +1,11 @@
 import os
 
 # image size tests
+os.system("python3 cnn_d1.py 0 1 90")
+os.system("python3 cnn_d1.py 0 2 90")
+os.system("python3 cnn_d1.py 0 4 90")
+os.system("python3 cnn_d1.py 0 8 90")
+
 os.system("python3 cnn_d1.py 2 2 32")
 os.system("python3 cnn_d1.py 2 2 64")
 os.system("python3 cnn_d1.py 2 2 90")
