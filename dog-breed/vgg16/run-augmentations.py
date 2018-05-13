e26a7a69517de7ee12ee964e9cb8f8c3b87444f4
jondo <jdombrowski416@gmail.com>
2018-05-13 10:59:58 -0700

    tests ran 5/12

--- dog-breed/vgg16/run-augmentations.py
+++ dog-breed/vgg16/run-augmentations.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('vg-low-data-low-aug.py')
-os.system('vg-low-data-high-aug.py')
-os.system('vg-high-data-low-aug.py')
-os.system('vg-high-data-high-aug.py')
+os.system("python3.6 vg-low-data-low-aug.py")
+os.system("python3.6 vg-low-data-high-aug.py")
+os.system("python3.6 vg-high-data-low-aug.py")
+os.system("python3.6 vg-high-data-high-aug.py")
